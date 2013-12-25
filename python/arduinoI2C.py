8c175951170e356cfab34459586b5a8b4be7f6bd
s8m1a5r3t0 <s8m1a5r3t0@gmail.com>
2013-12-25 21:50:11 +0800

    del no useful file

--- python/arduinoI2C.py
+++ python/arduinoI2C.py
@@ -1,3 +1,4 @@
+# -*- coding: cp950 -*-
 import smbus
 import time
 import subprocess
@@ -17,6 +18,36 @@ def readNumber():
     number = bus.read_byte(address)
     # number = bus.read_byte_data(address, 1)
     return number
+def readOldLogFile():
+    originalFile = open('/home/pi/PiAndCamera/haha/public/check.log', 'r')
+    while True:
+        i = originalFile.readline()
+        if i == '':
+            break
+        print('original Log: ' + i)
+    
+    originalFile.close()
+    return 0
+def writeNewLogFile():
+    newFile = open('/home/pi/PiAndCamera/haha/public/check.log', 'a')
+    from datetime import datetime  
+    now = datetime.now()  
+    year = now.year  
+    month = now.month  
+    day = now.day  
+    hour = now.hour  
+    minute = now.minute  
+    second= now.second
+
+    newFile.write(str(year)+' year ')
+    newFile.write(str(month)+' month ')
+    newFile.write(str(day)+' date ')
+    newFile.write(str(hour)+' hour ')
+    newFile.write(str(minute)+' minute ')
+    newFile.write(str(second)+' second\n')
+    
+    newFile.close()
+    return 0
 
 while True:
 #    var = input("Enter 1 - 9: ")
@@ -31,13 +62,18 @@ while True:
     number = readNumber()
     print "Pi Received light number from Arduino: ", number
     print
-    if number > 30 :
+    if number > 100 :
+        readOldLogFile()
+        writeNewLogFile()
+        
     	subprocess.call("date")
     	#raspistill -o pict.jpg -t 2000 
 	ts = int(time.time())
 	print ts
-		
+	strTS = str(ts)	
 	for x in range(10):
+            strX = str(x)	
+	    print strTS +" have take " + strX + " photo !!!!"	
             #subprocess.call(["command1", "arg1", "arg2"])
-            subprocess.call(["raspistill", "-o", ts + "_" + x +".jpg", "-t", "1", "-q", "50", "-w", "640", "-h", "640"])
+            subprocess.call(["raspistill", "-o", strTS + "_" + strX +".jpg", "-t", "1", "-q", "50", "-w", "640", "-h", "640"])
             time.sleep(1)   # sleep 1 sec
